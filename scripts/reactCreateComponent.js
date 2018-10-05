e3894e9b7771a0dbef5d75f57eb259809bcc72b5
Victor Beristain <vberistain@gmail.com>
2018-10-06 01:11:31 +0200

    Fix a bug regarding local paths

--- scripts/reactCreateComponent.js
+++ scripts/reactCreateComponent.js
@@ -16,6 +16,8 @@ const componentName = process.argv[2];
 const componentFolder = process.argv[3] || path.join('../', config.componentsFolder);
 const option = process.argv[4];
 
+const SCRIPT_PATH = process.cwd();
+
 if (!componentName) {
     console.log('Please enter a component name: rcc Header');
     process.exit(1);
@@ -29,7 +31,7 @@ const writeTemplate = (templateName, componentName, componentFolder, outputFileN
         'utf8'
     );
     template = template.replace(/COMPONENT_NAME/g, componentName);
-    const newPath = `${path.join(process.cwd(), `./${componentFolder}`)}`;
+    const newPath = `${path.join(SCRIPT_PATH, `./${componentFolder}`)}`;
     mkdirp.sync(newPath);
     if (fs.existsSync(`${newPath}/${outputFileName}`) && !willOverride) {
         console.log('\x1b[31m%s\x1b[0m', `File ${outputFileName} already exists in ${newPath}.`);
@@ -51,14 +53,15 @@ const writeTestTemplate = (componentName, componentFolder, outputFileName) => {
         path.join(__dirname, '../templates') + `/test.template.tsx`,
         'utf8'
     );
-    const testBasePath = path.join('../', config.testFolder);
+    const testBasePath = path.join('./', config.testFolder);
+
     const testPathArray = componentFolder.split('/');
     const i = testPathArray.indexOf('components');
     const testPath =
         testBasePath + testPathArray.slice(i + 1, testPathArray.length).join('/') !== ''
-            ? testPathArray.slice(i + 1, testPathArray.length).join('/') + '/'
-            : '';
-
+            ? testBasePath + '/' + testPathArray.slice(i + 1, testPathArray.length).join('/') 
+            : testBasePath;
+    console.log(testPathArray.slice(i + 1, testPathArray.length).join('/'))
     template = template.replace(/COMPONENT_NAME/g, componentName);
     template = template.replace(
         /COMPONENT_RELATIVE_PATH/g,
@@ -68,7 +71,7 @@ const writeTestTemplate = (componentName, componentFolder, outputFileName) => {
                 : ''
         }${componentName}`
     );
-    const newPath = `${path.join(process.cwd() + '/', `./${testPath}`)}`;
+    const newPath = `${path.join(SCRIPT_PATH, `./${testPath}`)}`;
     mkdirp.sync(newPath);
 
     if (fs.existsSync(`${newPath}/${outputFileName}`) && !willOverride) {
