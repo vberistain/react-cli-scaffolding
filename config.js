1f673f5f6de325096e19541cfca7c4538ef1bc8d
Victor Beristain <vberistain@gmail.com>
2018-10-09 20:26:08 +0200

    Fix config loading

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    componentsFolder: 'src/components',
+    testsFolder: 'test/functional/components',
+    overrideFiles: false,
+    styleFileExtension: 'css',
+    componentFileExtension: 'tsx',
+    templates: {
+        component: './templates/component.template',
+        style: './templates/style.template',
+        styleTypes: './templates/styletypes.template',
+        index: './templates/index.template',
+        test: './templates/test.template'
+    }
+};
